4a109d14857b62eea849017ee95045b4018f29e8
luca-masera <lucamasera1995@gmail.com>
2024-01-22 10:10:35 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <route>
+</template>
+<script>
+import axios from 'axios';
+import { store } from "./data/store.js";
+
+export default {
+  name: 'App',
+  components: {},
+
+  data() {
+
+    return {
+      store,
+    };
+  },
+  methods: {
+
+    getApiTest() {
+      axios.get(this.store.apiUrl + "projects").then((res) => {
+        console.log(res.data);
+      });
+    },
+  },
+
+  mounted() {
+    this.getApiTest;
+  },
+};
+</script>
+<style lang="scss" scoped>
+@use "./assets/styles/partials/variables.scss" as *;
+</style>
