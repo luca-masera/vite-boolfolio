6ff5cd17d98efeb264750237fa37d6efd0d4351f
luca-masera <lucamasera1995@gmail.com>
2024-01-22 22:34:23 +0100

    navbar con router-link

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,28 @@
 <template>
+  <AppHeader />
   <router-view></router-view>
 </template>
 
 <script>
 import axios from "axios";
 import { store } from "./store.js";
+import AppHeader from "./components/AppHeader.vue";
 
 export default {
   name: 'App',
-  components: {},
+
 
   data() {
 
     return {
-      store,
-      projects: [],
+
     };
   },
+
+  components: {
+    AppHeader
+  },
+
   methods: {
 
   },
@@ -28,9 +34,4 @@ export default {
 </script>
 <style lang="scss" scoped>
 @use "./assets/styles/partials/variables.scss" as *;
-
-h1 {
-  color: $font-color;
-}
 </style>
-./store.js
